057a4cd5afcf055ce9176a1eda9b6a1a0164f65d
IrrelevantGaymer <101901cora@gmail.com>
2025-05-24 01:36:09 -0700

    Added TODO

--- src/Parser/ParserError.hs
+++ src/Parser/ParserError.hs
@@ -2,6 +2,14 @@ module ParserError where
 
 import Lexer ( Atom )
 import Tokens ( Tkn )
+
+{-
+ - TODO: implement a FullResult sumtype that stores
+ - soft or hard errors, for easier error handling.
+ - We could differentiate between "Oh we tried to parse this but it didn't work"
+ - versus "We tried to parse this, and it was supposed ot be this but... 
+ - there was an error"
+ -}
 data ParserError = 
     UnexpectedToken { 
         expectedTkn :: Tkn, 
