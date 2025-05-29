5c9f674e0c1dee9cc51b4d872fc16d08b8d0f697
IrrelevantGaymer <101901cora@gmail.com>
2025-05-28 18:38:16 -0700

    Unfortunately order does matter, I might have to look into how modules work in Haskell

--- src/Lexer/_build.sh
+++ src/Lexer/_build.sh
@@ -1,4 +1,3 @@
 echo "building Lexer"
-# For some weird reason, when combining these commands using *.hs, we get a compiler error
 ghc-9.10.2 -Wall -fforce-recomp -c -O -odir ./build -hidir ./build src/Lexer/Tokens.hs
 ghc-9.10.2 -Wall -fforce-recomp -c -O -odir ./build -hidir ./build src/Lexer/Lexer.hs
