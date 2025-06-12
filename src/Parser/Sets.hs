dd96980acebc8c0b3f3df9e972ab77305433e46d
IrrelevantGaymer <101901cora@gmail.com>
2025-06-11 23:46:51 -0700

    Added Support for Built in Sets and also ungeneralized checking if a value is in a set (DRY is the devil)

--- src/Parser/Sets.hs
+++ src/Parser/Sets.hs
@@ -2,7 +2,7 @@
 module Sets where {
     import Pattern (Pat, getShape, Pattern (..), PatShape, PatternShape (..));
     import Text.Printf (printf);
-    
+
     type UnOp = UnarySetOperation;
     data UnarySetOperation = PowerSet deriving(Show);
     type BinOp = BinarySetOperation;
@@ -20,29 +20,38 @@ module Sets where {
         BinOpSet !BinOp !SetDef !SetDef |
         Id !SetDef deriving(Show);
 
+    data BuiltInSet = All;
+
+    getBuiltInSet :: String -> Maybe BuiltInSet;
+    getBuiltInSet "All" = Just All;
+    getBuiltInSet _     = Nothing;
+
     -- TODO: make this a HashMap instead
     type Sets = [(String, SetDef)];
 
-    inSetByPred :: Sets -> (Pat -> Bool) -> SetDef -> Bool;
-    inSetByPred _ f (Set pats) = or $ fmap f pats;
-    inSetByPred sets f (Word set) = 
-        or $ inSetByPred sets f . snd <$> findByKey fst set sets 
-    where {
-        findByKey :: Eq b => (a -> b) -> b -> [a] -> Maybe a;
-        findByKey _ _ [] = Nothing;
-        findByKey g k (x:xs)
-            | g x == k  = Just x
-            | otherwise = findByKey g k xs
-    };
-    inSetByPred _ _ (UnOpSet PowerSet _) = False;
-    inSetByPred sets f (BinOpSet Union setA setB) =
-        inSetByPred sets f setA || inSetByPred sets f setB;
-    inSetByPred sets f (BinOpSet Difference setA setB) =
-        not $ inSetByPred sets f setB && inSetByPred sets f setA;
-    inSetByPred _ _ (BinOpSet CartesianProduct _ _) = False;
-    inSetByPred _ _ (Id set) = error $ printf
-        "Id %s should only be used for parsing set expressions"
-        $ show set;
+    -- inSetByPred :: Sets -> (Pat -> Bool) -> SetDef -> Bool;
+    -- inSetByPred _ f (Set pats) = or $ fmap f pats;
+    -- inSetByPred sets f (Word set) 
+    --     | Just builtIn <- getBuiltInSet set = case builtIn of {
+    --         Any -> True;
+    --     }
+    --     | otherwise = or $ inSetByPred sets f . snd <$> findByKey fst set sets 
+    -- where {
+    --     findByKey :: Eq b => (a -> b) -> b -> [a] -> Maybe a;
+    --     findByKey _ _ [] = Nothing;
+    --     findByKey g k (x:xs)
+    --         | g x == k  = Just x
+    --         | otherwise = findByKey g k xs
+    -- };
+    -- inSetByPred _ _ (UnOpSet PowerSet _) = False;
+    -- inSetByPred sets f (BinOpSet Union setA setB) =
+    --     inSetByPred sets f setA || inSetByPred sets f setB;
+    -- inSetByPred sets f (BinOpSet Difference setA setB) =
+    --     not $ inSetByPred sets f setB && inSetByPred sets f setA;
+    -- inSetByPred _ _ (BinOpSet CartesianProduct _ _) = False;
+    -- inSetByPred _ _ (Id set) = error $ printf
+    --     "Id %s should only be used for parsing set expressions"
+    --     $ show set;
 
     data SetShape = SetRef SetDef | IdxInSetRef [Int] SetDef deriving (Show);
 
@@ -50,13 +59,46 @@ module Sets where {
     type Keys = [(String, SetShape)];
 
     valueInSet :: Sets -> Pat -> SetDef -> Bool;
-    valueInSet sets value = inSetByPred sets (value ==);
+    valueInSet _ value (Set pats) = value `elem` pats;
+    valueInSet sets value (Word set)
+        | Just All <- tryBuiltIn = True
+        | Nothing  <- tryBuiltIn = or $ valueInSet sets value . snd <$> findByKey fst set sets
+    where {
+        tryBuiltIn = getBuiltInSet set;
+
+        findByKey :: Eq b => (a -> b) -> b -> [a] -> Maybe a;
+        findByKey _ _ [] = Nothing;
+        findByKey g k (x:xs)
+            | g x == k  = Just x
+            | otherwise = findByKey g k xs
+    };
+    valueInSet _ _ (UnOpSet PowerSet _) = undefined;
+    valueInSet sets value (BinOpSet Union setA setB) =
+        valueInSet sets value setA || valueInSet sets value setB;
+    valueInSet sets value (BinOpSet Difference setA setB) =
+        not $ valueInSet sets value setB && valueInSet sets value setA;
+    valueInSet sets (Tuple values) set@(BinOpSet CartesianProduct _ _) =
+        or $ null <$> matchValuesWithSet values set
+    where {
+        matchValuesWithSet vs (BinOpSet CartesianProduct setA setB) = do {
+            vs' <- matchValuesWithSet vs setA;
+            matchValuesWithSet vs' setB;
+        };
+        matchValuesWithSet (v:vs) s
+            | valueInSet sets v s = Just vs
+            | otherwise           = Nothing;
+        matchValuesWithSet [] _   = Nothing;
+    };
+    valueInSet _ _ (BinOpSet CartesianProduct _ _) = False;
+    valueInSet _ _ (Id set) = error $ printf
+        "Id %s should only be used for parsing set expressions"
+        $ show set;
 
     valueInIdxSet :: Sets -> [Int] -> String -> SetDef -> Bool;
-    valueInIdxSet sets indices value = inSetByPred sets (valueIn indices value) where {
+    valueInIdxSet _ idcs value (Set pats) = or $ fmap (valueIn idcs value) pats  where {
         valueIn :: [Int] -> String -> Pat -> Bool;
-        valueIn (idx:idcs) k (Tuple vs) = or $ valueIn idcs k <$> vs !? idx;
-        valueIn (idx:idcs) k (List vs) = or $ valueIn idcs k <$> vs !? idx;
+        valueIn (i:is) k (Tuple vs) = or $ valueIn is k <$> vs !? i;
+        valueIn (i:is) k (List vs) = or $ valueIn is k <$> vs !? i;
         valueIn [] k (Value v) = k == v;
         valueIn [] k (Num v) = k == show v;
         valueIn _ _ _ = False;
@@ -69,6 +111,30 @@ module Sets where {
             | idx > 0   = xs !? (idx - 1)
             | otherwise = Nothing;
     };
+    valueInIdxSet sets idcs value (Word set)
+        | Just All <- tryBuiltIn = False -- The general pattern shape of All is Value not Tuple
+        | Nothing  <- tryBuiltIn = or $ valueInIdxSet sets idcs value . snd <$> findByKey fst set sets
+    where {
+        tryBuiltIn = getBuiltInSet set;
+
+        findByKey :: Eq b => (a -> b) -> b -> [a] -> Maybe a;
+        findByKey _ _ [] = Nothing;
+        findByKey g k (x:xs)
+            | g x == k  = Just x
+            | otherwise = findByKey g k xs
+    };
+    valueInIdxSet _ _ _ (UnOpSet PowerSet _) = undefined;
+    valueInIdxSet sets idcs value (BinOpSet Union setA setB) =
+        valueInIdxSet sets idcs value setA || valueInIdxSet sets idcs value setB;
+    valueInIdxSet sets idcs value (BinOpSet Difference setA setB) =
+        not $ valueInIdxSet sets idcs value setB && valueInIdxSet sets idcs value setA;
+    -- As far as I know, trying to check if an indexed value is in a
+    -- cartesian product set is useless, however it would be relatively easy
+    -- to implement.  There might be some crazy stuff with power sets later.
+    valueInIdxSet _ _ _ (BinOpSet CartesianProduct _ _) = undefined;
+    valueInIdxSet _ _ _ (Id set) = error $ printf
+        "Id %s should only be used for parsing set expressions"
+        $ show set;
 
     getPatternShape :: SetDef -> Sets -> Maybe PatShape;
     getPatternShape (Set pats) _ = Just $ mconcat $ fmap getShape pats;
@@ -133,11 +199,11 @@ module Sets where {
     where {
         defShape = getPatternShape def sets;
         getKeys ((idx, x):xs) idcs shp
-            | Value v <- x, V <- shp = ((v, IdxInSetRef (idcs ++ [idx]) def) :) 
+            | Value v <- x, V <- shp = ((v, IdxInSetRef (idcs ++ [idx]) def) :)
                 <$> getKeys xs idcs shp
             | Tuple vs <- x, T l shp' <- shp = if l == length vs then
                 getKeys (zip [0..] vs) (idcs ++ [idx]) shp'
-            else 
+            else
                 Nothing
             | List _ <- x, L _ <- shp = undefined
             | otherwise = Nothing;
