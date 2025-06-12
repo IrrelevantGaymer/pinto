8b9cc23b07a1a275a8b2536a3fa7c72cf948981a
IrrelevantGaymer <101901cora@gmail.com>
2025-06-12 01:41:58 -0700

    Added more messages to the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 set -e
+
 ./src/_build.sh
+
+echo "begin linking"
+
 ghc-9.10.2 -fforce-recomp -o pinto build/Lexer/*.o build/Parser/Rule/*.o build/Parser/*.o build/Interpreter/*.o build/*.o
+
+echo "finished building"
